5805af422eb973739727ebe454111b6c1e838d32
Abdou Aziz Fall <fallabdouaziz1996@mail.com>
2023-05-03 10:56:48 +0200

    api chatboot

--- src/hello.js
+++ src/hello.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/api', (req, res) => {
-    res.send("coucou")
+app.post('/api/', (req, res) => {
+   const message = req.body.message;
+   console.log(req.body.message)
+   if (message === 'Salut ça va ?') {
+       res.send('Très bien et toi ?')
+   } else {
+       res.send('Je n’ai pas compris !')
+   }
+
 });
 // starting the server
 app.listen(3000, () => {
