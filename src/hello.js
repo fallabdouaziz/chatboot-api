13091f59863a933a460ff586170c6b0760101457
Abdou Aziz Fall <fallabdouaziz1996@mail.com>
2023-05-03 10:28:48 +0200

    api chatboot

--- src/hello.js
+++ src/hello.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 
-app.use(bodyParser.json);
-app.use(cors);
-app.get('api/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send("coucou")
 });
 // starting the server
-app.listen(3001, () => {
-    console.log('listening on port 3001');
+app.listen(3000, () => {
+    console.log('listening on port 3000');
 });
 
